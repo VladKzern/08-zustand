c5e74d30ac3de902151fcd37345a5903a2e2239a
Vladyslav Korniienko <vladyslav.korniienko@stud.onu.edu.ua>
2025-09-08 21:03:40 +0300

    try to change url for vercel

--- utils/url.ts
+++ utils/url.ts
@@ -1,4 +1,4 @@
 export function absoluteUrl(path: string) {
-  const base = process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000";
+  const base = process.env.NEXT_PUBLIC_APP_URL ?? "https://08-zustand-eosin-nine.vercel.app/";
   return new URL(path, base).toString();
 }
